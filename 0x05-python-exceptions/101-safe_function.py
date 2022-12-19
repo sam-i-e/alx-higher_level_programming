7a4f26996fdb1e80c2a293cca8f660c05a2069ad
sam-i-e <samierapo@outlook.com>
2022-12-19 13:26:03 -0800

    python-exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-import sys
+# 102-magic_calculation.py
 
-def safe_function(fct, *args):
-    try:
-        result = fct(*args)
+
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / 
+            except:
+                result = b + a
+                break
         return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    ret = 0
-    for i in range(0, x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            ret += 1
-        except (ValueError, TypeError):
-            continue
-        print("")
-        return (ret)
+
+def safe_print_list(my_list=[], x=0):
+    idx = 0
+
+    try:
+        for i in my_list:
+            if idx < x:
+                print('{}'.format(my_list[idx]), end='')
+                idx += 1
+
+        print()
+    except TypeError:
+        pass
+    finally:
+        return 
